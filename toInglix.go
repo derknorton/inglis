e7d9a0daf0d0ac234670347890a68a6d3c5f636e
Derk Norton <derk.norton@gmail.com>
2023-03-27 08:00:37 -0600

    Added go program to translate English to Inglix.

---
+++ toInglix.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	bal "github.com/bali-nebula/go-component-framework/v2/bali"
+	osx "os"
+	sts "strings"
+	utf "unicode/utf8"
+)
+
+const alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+const dictionaryFile = "./dictionaries/English.bali"
+
+func notInAlphabet(r rune) bool {
+	return !sts.ContainsRune(alphabet, r)
+}
+
+func main() {
+	// Validate the commandline arguments.
+	if len(osx.Args) != 3 {
+		panic("toInglix <english file> <inglix file>")
+	}
+
+	// Read in the English text.
+	var bytes []byte
+	var err error
+	bytes, err = osx.ReadFile(osx.Args[1])
+	if err != nil {
+		panic(err)
+	}
+	var english = string(bytes)
+
+	// Load in the dictionary.
+	bytes, err = osx.ReadFile(dictionaryFile)
+	if err != nil {
+		panic(err)
+	}
+	var dictionary = bal.ParseDocument(bytes).ExtractCatalog()
+
+	// Translate to Inglix text.
+	var builder sts.Builder
+	var index = 0
+	var size = len(english)
+	for index < size {
+		var r, length = utf.DecodeRune([]byte(english[index:]))
+		if notInAlphabet(r) {
+			builder.WriteRune(r)
+			index += length
+			continue
+		}
+		var next = index + sts.IndexFunc(english[index:], notInAlphabet)
+		var word = bal.Quote(`"` + english[index:next] + `"`)
+		var translation = word
+		var value = dictionary.GetValue(word)
+		if value == nil {
+			var empty = bal.Component(bal.Quote(`""`))
+			dictionary.SetValue(word, empty)
+		} else {
+			translation = value.ExtractQuote()
+			if translation.IsEmpty() {
+				translation = word
+			} else {
+			}
+		}
+		builder.WriteString(translation.AsString())
+		index = next
+	}
+	var inglix = builder.String()
+
+	// Write out the Inglix text.
+	err = osx.WriteFile(osx.Args[2], []byte(inglix), 0644)
+	if err != nil {
+		panic(err)
+	}
+
+	// Write out the updated dictionary.
+	err = osx.WriteFile(dictionaryFile, bal.FormatDocument(bal.Component(dictionary)), 0644)
+	if err != nil {
+		panic(err)
+	}
+}
